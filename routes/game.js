4571e45ae354b8b21d44570d43c8c578d642bcc9
ibra <lomeseth1@gmail.com>
2023-09-12 19:42:17 +0300

    fix some dlcs don't have a explict price

--- routes/game.js
+++ routes/game.js
@@ -25,26 +25,37 @@ router.get("/", async (req, res, next) => {
             data = await data.json();
             data = data[id].data;
             if (data.type === "game") {
+                if (!data.is_free || data.price_overview.initial) {
+                    res.json("WE DON'T SUPPORT THIS TYPE OF GAME");
+                    return;
+                }
                 let obj = {
                     _id: id, type: 0, name: data.name, original_price: data.is_free ? 0 : data.price_overview.initial, header_image: data.header_image, desc: data.short_description, developers: data.developers, publishers: data.publishers, score: data.metacritic?.score, screenshots: data?.screenshots?.map(screen => (screen.path_full)), movies: data?.movies?.map((movie) => (movie.webm.max)), releaseDate: { coming_soon: data.release_date.coming_soon, date: new Date(data.release_date.date) }, genres: data.genres?.map(genre => (genre.description)), dlc: data.dlc
                 }
                 let dlcs = [];
                 if (data.dlc) {
-                    let prs = data.dlc.map(async (id) => {
+                    let prs = data.dlc.map(async (id, index) => {
                         increaseRequests(next);
                         let data = await fetch(`http://store.steampowered.com/api/appdetails?appids=${id}&l=english`);
                         data = await data.json();
                         data = data[id].data;
+                        if (!data.is_free || data.price_overview.initial) {
+                            return;
+                        }
                         let dlc = {
                             _id: id, type: 1, name: data.name, original_price: data.is_free ? 0 : data.price_overview.initial, header_image: data.header_image, desc: data.short_description, developers: data.developers, publishers: data.publishers, score: data.metacritic?.score, screenshots: data.screenshots?.map(screen => (screen.path_full)), movies: data.movies?.map((movie) => (movie.webm["480"])), releaseDate: { coming_soon: data.release_date.coming_soon, date: new Date(data.release_date.date) }, genres: data.genres?.map(genre => (genre.description))
                         }
                         dlcs.push(dlc)
                         await Game.findOneAndUpdate({ _id: id }, dlc, { upsert: true });
                     })
+
                     await Promise.all(prs);
-                    Game.findOneAndUpdate({ _id: id }, obj, { upsert: true }).exec();
                     obj.dlc = dlcs;
                     res.json(obj);
+                    obj.dlc = obj.dlc.map(item => {
+                        return item._id;
+                    })
+                    Game.findOneAndUpdate({ _id: id }, obj, { upsert: true }).exec();
                 }
                 else {
 
