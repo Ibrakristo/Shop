29670175b7ec6f8fb6bef8f8c301781cc6340bbb
ibra <lomeseth1@gmail.com>
2023-09-12 17:53:02 +0300

    demo

--- routes/game.js
+++ routes/game.js
@@ -32,11 +32,11 @@ router.get("/", async (req, res, next) => {
                 if (data.dlc) {
                     let prs = data.dlc.map(async (id) => {
                         increaseRequests(next);
-                        let data = await fetch(`http://store.steampowered.com/api/appdetails?appids=${id}`);
+                        let data = await fetch(`http://store.steampowered.com/api/appdetails?appids=${id}&l=english`);
                         data = await data.json();
                         data = data[id].data;
                         let dlc = {
-                            _id: id, type: 1, name: data.name, original_price: data.is_free ? 0 : data.price_overview.initial, header_image: data.header_image, desc: data.short_description, developers: data.developers, publishers: data.publishers, score: data.metacritic?.score, screenshots: data.screenshots?.map(screen => (screen.path_full)), movies: data.movies?.map((movie) => (movie.webm.max)), releaseDate: { coming_soon: data.release_date.coming_soon, date: new Date(data.release_date.date) }, genres: data.genres?.map(genre => (genre.description))
+                            _id: id, type: 1, name: data.name, original_price: data.is_free ? 0 : data.price_overview.initial, header_image: data.header_image, desc: data.short_description, developers: data.developers, publishers: data.publishers, score: data.metacritic?.score, screenshots: data.screenshots?.map(screen => (screen.path_full)), movies: data.movies?.map((movie) => (movie.webm["480"])), releaseDate: { coming_soon: data.release_date.coming_soon, date: new Date(data.release_date.date) }, genres: data.genres?.map(genre => (genre.description))
                         }
                         dlcs.push(dlc)
                         await Game.findOneAndUpdate({ _id: id }, dlc, { upsert: true });
